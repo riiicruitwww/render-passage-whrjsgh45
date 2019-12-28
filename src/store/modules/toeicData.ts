ddd07356395993d05a6c888cf2687b86f8bece57
조건호 <zgh9807@gmail.com>
2019-12-29 01:31:57 +0900

    paser code 이동 및 update 함수 수정

--- src/store/modules/toeicData.ts
+++ src/store/modules/toeicData.ts
@@ -2,7 +2,7 @@
 export interface ToeicData {
     toeicPart6s: Array<ToeicPart6>,
     pageState: string,
-    chooseInfo: Array<ChooseData>
+    choiceList: Array<Choice>
 }
 
 export interface ToeicPart6 {
@@ -33,8 +33,8 @@ export interface Voca {
     meaning: string
 }
 
-export interface ChooseData {
-    click: boolean,
+export interface Choice {
+    isSelected: boolean,
     qId: string,
     chooseAnswer: string,
     correctAnswer: string,
@@ -66,14 +66,15 @@ interface ChangePageState {
 interface CreateChooseInfo {
     type: typeof CREATE_CHOOSE_INFO;
     meta: {
-        chooseInfo: Array<ChoiceArea>
+        choiceList: Array<Choice>
     }
 }
 
 interface UpdateChooseInfo {
     type: typeof UPDATE_CHOOSE_INFO;
     meta: {
-        chooseInfo: Array<ChoiceArea>
+        qId: string,
+        orderValue: string
     }
 }
 
@@ -104,24 +105,271 @@ function changePageState(pageState: string) {
     }
 }
 
-function createChooseInfo(chooseInfo: Array<ChooseData>) {
+function createChooseInfo(choiceList: Array<Choice>) {
 
     return {
         type: CREATE_CHOOSE_INFO,
         meta: {
-            chooseInfo: chooseInfo
+             choiceList: choiceList
         }
     }
 }
 
-function updateChooseInfo(chooseInfo: Array<ChooseData>) {
+function updateChooseInfo(qId: string, orderValue: string) {
 
     return {
         type: UPDATE_CHOOSE_INFO,
         meta: {
-            chooseInfo: chooseInfo
+            qId: qId,
+            orderValue: orderValue
         }
     }
+}
+
+
+export function jsonParser(jsonData: object): Array<ToeicPart6> {
+    let boxIdArray = [];
+    let ToeicPart6List: Array<ToeicPart6> = [];
+    const toeicPart6: ToeicPart6 = {
+        name: '',
+        type: '',
+        preview: '',
+        questions: [],
+        voca: []
+    };
+
+    for(let key in jsonData) {
+        if(jsonData.hasOwnProperty(key)) {
+            switch(key) {
+                case 'name': toeicPart6.name = jsonData[key];
+                    break;
+                case 'type': toeicPart6.type = jsonData[key];
+                    break;
+                case 'preview': toeicPart6.preview = jsonData[key];
+                    break;
+                case 'questions': {
+                    let index = 0, bIndex = 0;
+                    let subJsonData = jsonData[key];
+                    const questions: Array<QuestionsData> = [];
+                    let data: QuestionsData = {
+                        id: '',
+                        packType: '',
+                        passageBoxId: '',
+                        correctAnswer: '',
+                        order: '',
+                        choiceArea: [],
+                        studyNote: ''
+                    };
+
+                    for(let i=0; i<subJsonData.length; i++) {
+
+                        data = {
+                            id: '',
+                            packType: '',
+                            passageBoxId: '',
+                            correctAnswer: '',
+                            order: '',
+                            choiceArea: [],
+                            studyNote: ''
+                        };
+
+                        const subJsonDataValue = subJsonData[i];
+                        for(let key in subJsonDataValue) {
+                            if(subJsonDataValue.hasOwnProperty(key)) {
+                                switch (key) {
+                                    case 'id':
+                                        data.id = subJsonDataValue[key];
+                                        break;
+                                    case 'pack_type':
+                                        data.packType = subJsonDataValue[key];
+                                        break;
+                                    case 'passage_box_id': {
+                                        data.passageBoxId = subJsonDataValue[key];
+                                        boxIdArray[bIndex++] = subJsonDataValue[key];
+                                    }
+                                        break;
+                                    case 'correct_answer':
+                                        data.correctAnswer = subJsonDataValue[key];
+                                        break;
+                                    case 'order':
+                                        data.order = subJsonDataValue[key];
+                                        break;
+                                    case 'view_tree': {
+                                        let viewTree = subJsonDataValue[key];
+                                        for(key in viewTree) {
+                                            if(viewTree.hasOwnProperty(key)) {
+                                                if(key === 'children') {
+                                                    let children = viewTree[key];
+                                                    for(let i=0; i<children.length; i++) {
+
+                                                        let nameType = '';
+                                                        let childrenJsonData = children[i];
+                                                        for(key in childrenJsonData) {
+                                                            if(childrenJsonData.hasOwnProperty(key)) {
+                                                                if(key === 'name') {
+                                                                    nameType = childrenJsonData[key];
+                                                                }
+                                                                else if(key === 'children') {
+
+                                                                    if(nameType === 'blank')
+                                                                    {
+                                                                        let studyNote = '';
+                                                                        const subChildren = childrenJsonData[key];
+                                                                        for(let j=0; j<subChildren.length; j++) {
+                                                                            let chunkId = '';
+                                                                            const subChildrenJsonData = subChildren[j];
+                                                                            for(key in subChildrenJsonData) {
+                                                                                if(subChildrenJsonData.hasOwnProperty(key)) {
+                                                                                    if(key === 'chunk_id') {
+                                                                                        chunkId = subChildrenJsonData[key];
+                                                                                        studyNote += jsonData['chunk_map'][chunkId]['text_en'];
+                                                                                        studyNote += '\\n';
+                                                                                        studyNote += jsonData['chunk_map'][chunkId]['text_kr'];
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                            studyNote += '\\n';
+                                                                            studyNote += '\\n';
+                                                                        }
+                                                                        data.studyNote = studyNote;
+                                                                    }
+                                                                    else if(nameType === 'choice_area')
+                                                                    {
+                                                                        let cIndex = 0;
+                                                                        const subChildren = childrenJsonData[key];
+                                                                        for(let j=0; j<subChildren.length; j++) {
+                                                                            let choiceArea: ChoiceArea = {
+                                                                                number: '',
+                                                                                numberValue: ''
+                                                                            };
+                                                                            const subChildrenJsonData = subChildren[j];
+                                                                            for(key in subChildrenJsonData) {
+                                                                                if(subChildrenJsonData.hasOwnProperty(key)) {
+                                                                                    if(key === 'number') {
+                                                                                        choiceArea.number = subChildrenJsonData[key];
+                                                                                    } else if(key === 'children') {
+                                                                                        const subSubChildren = subChildrenJsonData[key];
+                                                                                        for(let k=0; k<subSubChildren.length; k++) {
+                                                                                            const subSubChildrenJsonData = subSubChildren[k];
+                                                                                            for(key in subSubChildrenJsonData) {
+                                                                                                if(subSubChildrenJsonData.hasOwnProperty(key)) {
+                                                                                                    if(key === 'children') {
+                                                                                                        const subSubSubChildren = subSubChildrenJsonData[key];
+                                                                                                        for(let l=0; l<subSubSubChildren.length; l++) {
+                                                                                                            const subSubSubChildrenJsonData = subSubSubChildren[l];
+                                                                                                            for(key in subSubSubChildrenJsonData) {
+                                                                                                                if(subSubSubChildrenJsonData.hasOwnProperty(key)) {
+                                                                                                                    if(key === 'chunk_id') {
+                                                                                                                        const chunkId = subSubSubChildrenJsonData[key];
+                                                                                                                        choiceArea.numberValue = jsonData['chunk_map'][chunkId]['text_en'];
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                            data.choiceArea[cIndex++] = choiceArea;
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                        break;
+                                }
+                            }
+                        }
+                        questions[index++] = data;
+                    }
+                    toeicPart6.questions = questions;
+                    boxIdArray = Array.from(new Set(boxIdArray));
+                }
+                    break;
+                case 'passage_box': {
+                    let index = 0;
+                    let passagesIdList = [];
+                    let subJsonData = jsonData[key];
+                    const vocaList: Array<Voca> = [];
+                    let data: Voca = { word: '', meaning: '' };
+
+                    for(let key in subJsonData) {
+                        if(subJsonData.hasOwnProperty(key)) {
+                            if(key === 'passages') {
+
+                                let passagesId = '';
+                                let passageBoxId = '';
+                                let subSubJsonData = subJsonData[key];
+
+                                for(let i=0; i<subSubJsonData.length; i++) {
+
+                                    let subSubJsonDataValue = subSubJsonData[i];
+                                    for(let key in subSubJsonDataValue) {
+                                        if(subSubJsonDataValue.hasOwnProperty(key)) {
+                                            if(key === 'id') { passagesId = subSubJsonDataValue[key]; }
+                                            else if(key == 'passage_box_id') { passageBoxId = subSubJsonDataValue[key]; }
+                                        }
+                                    }
+
+                                    for(let j=0; j<boxIdArray.length; j++) {
+
+                                        if(passageBoxId === boxIdArray[j]) {
+                                            passagesIdList[index++] = passagesId;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    index = 0;
+                    passagesIdList = Array.from(new Set(passagesIdList));
+                    for(let key in subJsonData) {
+                        if(subJsonData.hasOwnProperty(key)) {
+                            if(key === 'vocabularies') {
+                                let subSubJsonData = subJsonData[key];
+
+                                for(let i=0; i<subSubJsonData.length; i++) {
+
+                                    let passagesId = '';
+                                    data = {word: '', meaning: ''};
+                                    let subSubJsonDataValue = subSubJsonData[i];
+
+                                    for(let key in subSubJsonDataValue) {
+                                        if(subSubJsonDataValue.hasOwnProperty(key)) {
+                                            if(key === 'passage_id') { passagesId = subSubJsonDataValue[key]; }
+                                            else if(key === 'word') { data.word = subSubJsonDataValue[key]; }
+                                            else if(key === 'meaning') { data.meaning = subSubJsonDataValue[key]; }
+                                        }
+                                    }
+
+                                    for(let j=0; j<passagesIdList.length; j++) {
+                                        if(passagesId === passagesIdList[j]) {
+                                            vocaList[index++] = data;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    toeicPart6.voca = vocaList;
+                }
+                    break;
+            }
+        }
+    }
+    ToeicPart6List[0] = toeicPart6;
+    return ToeicPart6List;
 }
 
 // Action 함수 정의
@@ -136,7 +384,7 @@ export const actionCreators = {
 const initialState: ToeicData = {
     toeicPart6s: [],
     pageState: 'exam',
-    chooseInfo: []
+    choiceList: []
 };
 
 export function toeicData(state = initialState, action: ActionTypes) {
@@ -155,12 +403,19 @@ export function toeicData(state = initialState, action: ActionTypes) {
         case CREATE_CHOOSE_INFO:
             return {
                 ...state,
-                chooseInfo: action.meta.chooseInfo
+                choiceList: action.meta.choiceList
             };
         case UPDATE_CHOOSE_INFO:
             return {
                 ...state,
-                chooseInfo: action.meta.chooseInfo
+                choiceList: state.choiceList.map(key => {
+                    if(key.qId === action.meta.qId) {
+                        key.isSelected = true;
+                        key.chooseAnswer = action.meta.orderValue
+                    }
+
+                    return key;
+                })
             };
         default:
             return state;

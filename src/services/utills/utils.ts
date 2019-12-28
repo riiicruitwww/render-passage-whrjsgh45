e19e25c89bc786da636b00dc0a09932fd9e70b2f
조건호 <zgh9807@gmail.com>
2019-12-29 01:37:43 +0900

    함수 수정

--- src/services/utills/utils.ts
+++ src/services/utills/utils.ts
@@ -1,25 +1,12 @@
+import {ChoiceArea, QuestionsData, ToeicPart6, Voca} from "../../store/modules/toeicData";
 
-const Utils = () => {
-
-    const numberToABC = (number: string, bType: string): string => {
-        /*
-            bType = S: square bracket => [ ], return '[a]';
-            bType = C: curly brackets => { }, return '{a}';
-            bType = P: parentheses => ( ), return '(a)';
-            bType = '': not bracket, return 'a';
-         */
-        const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        return bType === 'S' ? '[' + alphabet[Number(number)] + ']'
-            : bType === 'C' ? '{' + alphabet[Number(number)] + '}'
-                : bType === 'P' ? '(' + alphabet[Number(number)] + ')'
-                    : alphabet[Number(number)];
-    };
-
-    return {
-        numberToABC: (number: string, bType: string): string => {
-            return numberToABC(number, bType);
-        }
-    }
+export const numberToABC = (number: string): string => {
+    /*
+        bType = S: square bracket => [ ], return '[a]';
+        bType = C: curly brackets => { }, return '{a}';
+        bType = P: parentheses => ( ), return '(a)';
+        bType = '': not bracket, return 'a';
+     */
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    return alphabet[Number(number)];
 };
-
-export default Utils();
